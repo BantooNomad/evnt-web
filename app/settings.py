d51c622b36ceae38d6d9ff4e0e412a2e6928ad9f
Ian Juma <ian.juma@yahoo.com>
2014-08-02 14:44:33 +0300

    settings user

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,3 @@ SECRET_KEY=secret
 CSRF_ENABLED=True
 CSRF_SESSION_LKEY='dev_key_h8asSNJ9s9=+'
 THREADED = False
-
-CLIENT_ID = '20567748533.apps.googleusercontent.com'
-CLIENT_SECRET = '82Y0LzHkrfXlep81sVLWIWh2'
-SCOPE = 'https://medtest21.appspot.com/oauth2callback'
